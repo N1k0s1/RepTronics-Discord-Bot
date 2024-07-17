744b2875cb6e48ef680b5f210fba7b579c4525a0
Niko <128133376+N1k0s1@users.noreply.github.com>
2024-07-17 21:56:51 +1200

    Worked on the embeds and the data structure.
    Worked on the embeds and the data structure.
    Just need to work some more on the data then I can start working on commands again

--- embed.py
+++ embed.py
@@ -5,23 +5,25 @@ with open('data.json') as f:
 
 def genembed(title, version):
         # Define the embed1 variable
-        embed1 = discord.Embed(title, color=discord.Color.blue())
+        embed1 = discord.Embed(title=title, color=discord.Color.blue())
         product = data['versions'][version]
 
         embed1.add_field(name="Chip Models", value=product['chipmodels'], inline=False)
         embed1.add_field(name="Features", value="\n".join(product['features']), inline=False) 
         embed1.add_field(name="Price", value=product['price'], inline=True)
-        embed1.add_field(name="Notes", value=product['note'], inline=True)
+        embed1.add_field(name="General Features", value=product['note'], inline=True)
 
         # Add seller fields
         for seller_name in product['sellers']:
             seller = data['sellers'][seller_name]
             embed1.add_field(name=seller_name, value=seller['store_link'], inline=True)
+        return embed1
 
 def sellerembed(title, seller):
-        embed = discord.Embed(title="HiCity", color=discord.Color.blue())
+        embed = discord.Embed(title, color=discord.Color.blue())
         seller = data['sellers'][seller]
 
         embed.add_field(name="WhatsApp", value=seller['whatsapp'], inline=False)
         embed.add_field(name="Discord", value=seller['discord'], inline=False)
         embed.add_field(name="Website", value=seller['store_link'], inline=False)
+        return embed
