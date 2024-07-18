3347fb4a04806c939077bf5270109d056f5a92bf
Niko <128133376+N1k0s1@users.noreply.github.com>
2024-07-18 20:00:11 +1200

    Got more then 1 button working!
    Additionally, I updated the data.json file, and just need to spend a bit more time on it before I can start work on the commands again.

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ bot = discord.Bot()
 import json 
 with open('data.json') as f:
     data = json.load(f)
-
+from embed import genembed, sellerembed
 
 @bot.event
 async def on_ready():
@@ -57,29 +57,23 @@ class Gen2View(discord.ui.View):
     def __init__(self):
         super().__init__()
 
-    @discord.ui.button(label='Button 1', custom_id='button1')
+    @discord.ui.button(label='JL', custom_id='JL')
     async def on_button1(self, button: discord.ui.Button, interaction: discord.Interaction):
-        # Define the embed1 variable
-        embed1 = discord.Embed(title="Gen 2", color=discord.Color.blue())
-        product = data['versions']['gen2']
+        embed1 = genembed("Gen 2 | JL", "gen2jl")
+        await interaction.response.send_message('JL', embed=embed1, ephemeral=True)
 
-        embed1.add_field(name="Chip Models", value=product['chipmodels'], inline=False)
-        embed1.add_field(name="Features", value="\n".join(product['features']), inline=False) 
-        embed1.add_field(name="Price", value=product['price'], inline=True)
-        embed1.add_field(name="Notes", value=product['note'], inline=True)
-
-        # Add seller fields
-        for seller_name in product['sellers']:
-            seller = data['sellers'][seller_name]
-            embed1.add_field(name=seller_name, value=seller['store_link'], inline=True)
-
-        # Send the first embed
-        await interaction.response.send_message('Button 1', embed=embed1, ephemeral=True)
-
+    @discord.ui.button(label='Airoha 1562U', custom_id='1562U')
+    async def on_button2(self, button: discord.ui.Button, interaction: discord.Interaction):
+        embed1 = genembed("Gen 2 | Airoha 1562U", "gen21562u")
+        await interaction.response.send_message('1562U', embed=embed1, ephemeral=True)
     # Repeat for the other buttons
 
 @bot.slash_command(name="gen2test", description="Show the gen 2 menu", guild_id = id)
 async def gen2test(ctx):
-    await ctx.respond('Choose an option:', view=Gen2View())
+    await ctx.respond('Gen 2 chips - Choose an option:', view=Gen2View())
+
+
+
+
 
 bot.run('')
